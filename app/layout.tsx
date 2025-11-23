6ce056950ee2b313badc915142d1b0438d665d4e
frooos9199 <47955716+frooos9199@users.noreply.github.com>
2025-11-23 08:36:43 +0300

    تغيير اسم صفحة not-found إلى not-found.jsx لحل مشكلة البناء

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Montserrat, Roboto } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import AuthProvider from "@/components/AuthProvider";
-import BottomNav from "@/components/BottomNav";
+
 
 const montserrat = Montserrat({ 
   subsets: ["latin"],
@@ -36,7 +36,6 @@ export default function RootLayout({
         <AuthProvider>
           <LanguageProvider>
             {children}
-            <BottomNav />
           </LanguageProvider>
         </AuthProvider>
       </body>
