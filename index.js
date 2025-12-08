fa760c0b7dc47d173916875a1474cf00528b3d89
frooos9199 <47955716+frooos9199@users.noreply.github.com>
2025-12-08 20:47:56 +0300

    Add background message handler for push notifications when app is closed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 import '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
 /**
  * @format
  */
@@ -7,5 +8,11 @@ import '@react-native-firebase/app';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+// Background Message Handler - للإشعارات والتطبيق مغلق
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('[BACKGROUND] Message received:', remoteMessage);
+  // يمكنك إضافة معالجة إضافية هنا
+});
 
 AppRegistry.registerComponent(appName, () => App);
