ea48045214112465a433fe7047c8031cf4e64c25
frooos9199 <47955716+frooos9199@users.noreply.github.com>
2025-12-19 19:19:45 +0300

    fix: remove duplicate code from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
@@ -12,4 +11,17 @@ export default defineConfig({
 		url: env("DATABASE_URL"),
 	},
 });
->>>>>>> 5f4758b (resolve conflicts and add DATABASE_URL to prisma schema)
+
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+	schema: "prisma/schema.prisma",
+	migrations: {
+		path: "prisma/migrations",
+	},
+	engine: "classic",
+	datasource: {
+		url: env("DATABASE_URL"),
+	},
+});
